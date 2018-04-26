37009841df7cd8744ec126bedb789b2c264794ad
Coşkun Kurtuldu <coskun.kurtuldu@codevist.com>
2018-04-26 15:44:30 +0300

    Init Commit

---
+++ ccors-transactionresult-service-net/CreditCardPaymentResultService.asmx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace ccors_transactionresult_service_net
+{
+    /// <summary>
+    /// Summary description for CreditCardPaymentResultService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CreditCardPaymentResultService : System.Web.Services.WebService
+    {
+
+        [WebMethod(EnableSession = true)]
+        public Response ReceiveTransactionResult(int StatusCode, string ResultCode, string ResultMessage, string OrderId, string MPAY, double Price, DateTime TransactionDate, string ServiceTypeCode, string TransactionTypeCode, string ExtraParam)
+        {
+            Response response = new Response();
+            response.StatusCode = 0;
+            response.ResultCode = string.Empty;
+            response.ResultMessage = string.Empty;
+            return response;
+        }
+    }
+}
---
+++ ccors-transactionresult-service-net/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ccors_transactionresult_service_net
+{
+    [Serializable]
+    public class Response
+    {
+        public int StatusCode { get; set; }
+        public string ResultCode { get; set; }
+        public string ResultMessage { get; set; }
+    }
+}
